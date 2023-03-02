c2f31f578c5c32c8f53609695063ecef4ade6da0
GrigorijsPerec <67803186+GrigorijsPerec@users.noreply.github.com>
2023-03-02 14:58:04 +0200

    Update main_lessson_4.go

--- main_lessson_4.go
+++ main_lessson_4.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func task1() {
@@ -17,10 +18,44 @@ func task1() {
 	}
 }
 
-func task2() {
+func main() {
+	var N, x, y int
+	fmt.Print("Введите число N: ")
+	fmt.Scan(&N)
 
+	max := int(math.Sqrt(float64(N))) 
+	found := false                    
+
+	for x = 1; x <= max; x++ {
+		for y = 1; y <= max; y++ {
+			if x*x+y*y == N {
+				fmt.Printf(x, y)
+				found = true
+			}
+		}
+	}
+
+	if !found {
+		fmt.Println("Решений нет")
+	}
+}
+
+func task2() {
+	var n, reversed int
+	fmt.Print("Введите натуральное число: ")
+	fmt.Scan(&n)
+
+	for n > 0 {
+		remainder := n % 10
+		reversed = reversed*10 + remainder
+		n /= 10
+	}
+
+	fmt.Printf("Перевернутое число: %d\n", reversed)
 }
 
 func main() {
 	task1()
+	task2()
+	task3()
 }
